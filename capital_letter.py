cadb88527c2e2415655153dbd486afdeddea62f3
wehute <125381586+wehute@users.noreply.github.com>
2024-03-06 22:51:34 +0700

    Заглавная буква

---
+++ capital_letter.py
@@ -0,0 +1,10 @@
+text = 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. euismod faucibus rutrum sollicitudin ex purus faucibus aliquam ipsum tellus etiam blandit dictumst. vestibulum vel lorem lectus finibus eros tempus laoreet condimentum porttitor taciti nec hac. faucibus quam feugiat mauris venenatis auctor aliquet quis interdum consectetur convallis proin. proin dui condimentum vitae conubia nec justo at mollis aliquam ad vitae. luctus ante donec dictum ultrices ligula volutpat eget dolor tortor eros finibus habitant habitant. curabitur quis cras sapien dapibus phasellus. maximus metus taciti vestibulum litora dolor metus eu fames libero volutpat nullam lacinia condimentum nibh. amet aliquet semper at nullam cubilia erat lorem eros morbi fringilla fusce nullam. non suscipit vel ut consequat justo nisl consectetur taciti ipsum enim suscipit vehicula. metus consequat vulputate enim lacus habitant. ullamcorper massa maecenas arcu ultricies luctus sociosqu placerat sodales. pretium donec enim ut mauris eget maximus. quam cras conubia congue erat maecenas. Netus lacinia auctor id venenatis tellus blandit felis mattis. nostra vestibulum lacinia rhoncus metus suspendisse volutpat curae proin. convallis leo porta posuere mi proin suspendisse quisque conubia non duis mollis. pharetra aliquet risus eleifend dolor velit viverra class consequat scelerisque feugiat eu ornare. lobortis nisl scelerisque sapien placerat ultrices feugiat venenatis habitant cras et quisque neque ex. sem lacinia purus dui cursus ullamcorper venenatis massa faucibus vivamus donec pulvinar dictum lacinia. etiam mi volutpat phasellus inceptos luctus primis duis orci ex porta. feugiat lacinia urna cubilia fusce odio habitasse ullamcorper sit adipiscing erat pharetra.'
+
+s = (text.upper()).split('. ')
+
+for i in range(0, len(s)):
+    x = s[i]
+    s[i] = x[:1] + x[1:].lower()
+
+new_text = '. '.join(s)
+print(new_text)
